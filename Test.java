095a75f652d2a40aa413e5c886fb010ba40b3f86
GitTestert <mamatha.konda.qa@gmail.com>
2021-02-17 17:08:35 +0530

    This is 2nd round, added test2.class modified test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,3 @@
 //This is newly added file from github from somebody else in team
+
+// updated modified codes pulled from github file
---
+++ src/test/java/Practice/CICDTest/Test2.java
@@ -0,0 +1,7 @@
+package Practice.CICDTest;
+
+public class Test2 {
+	
+	// this is newly added testcase aftew push and pull
+
+}
